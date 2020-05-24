1e68bb99c6e4250540ba5bc58be53868251bb549
Alexandre P <1724149+zaggash@users.noreply.github.com>
2020-05-24 17:32:51 +0200

    Adding comment during the build

--- run.sh
+++ run.sh
@@ -2,26 +2,31 @@
 set -e
 
 # * Make a copy so we never alter the original
+echo "* Copy PKGBUILD ..."
 cp -R /pkg /tmp/pkg
 cd /tmp/pkg
 
 # * makepkg -s cannot install AUR deps !
 # * Install (official repo + AUR) dependencies using yay.
+echo "* Install yay-bin from AUR ..."
 yay -Sy --noconfirm \
     $(pacman --deptest $(source ./PKGBUILD && echo ${depends[@]} ${makedepends[@]}))
 
 # * If env $CHECKSUM_SRC, add the checksum
 if [[ "$CHECKSUM_SRC" == true  ]]
 then
+  echo "* Add Checksum to PKGBUILD ..."
   makepkg -g >> ./PKGBUILD
 fi
 
 # * Run the build
+echo "* Run the Build ..."
 makepkg -f
 
 # * If $EXPORT_PKG, set permissions like the PKGBUILD file and export the package
 if [[ "$EXPORT_PKG" == true ]]
 then
+    echo "* Copy back the builded package ..."
     sudo chown $(stat -c '%u:%g' /pkg/PKGBUILD) ./*.pkg.tar.xz
     sudo mv ./*.pkg.tar.xz /pkg
 fi
